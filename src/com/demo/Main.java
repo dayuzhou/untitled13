ff9d30e2d3c708454d0cff0fe0d4ea0ee3d4d68e
dayuzhou <2507356905@qq.com>
2019-10-31 14:31:21 +0800

    Initial commit

---
+++ src/com/demo/Main.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for(int i=1;i<=9;i++){
+	    for (int a=1;a<=i;a++){
+	       System.out.print(a+"*"+i+"="+(a*i)+"\t");
+
+        }
+	    System.out.println();
+    }
+        // write your code here
+    }
+}
